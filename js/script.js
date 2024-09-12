8aa5af70580fab8be3dea736e02a4ff7c92499a8
ckru12244 <ckru12244@gmail.com>
2024-09-12 16:53:27 +0900

    nav 접근성

--- js/script.js
+++ js/script.js
@@ -27,4 +27,27 @@ $(function(){
 		$(".slider li").removeClass("current")
 		$(".slider li").eq(n).addClass("current")
 	});
+
+	$("nav > ul > li > a").hover(
+		function(){
+			$("nav > ul").addClass("active");
+		},
+		function(){
+			$("nav > ul").removeClass("active");
+		}
+	);
+	$("nav > ul > li > a").focusin(function(){
+		if($(this).parent().index() == 0){
+			$("nav > ul").addClass("active");
+		}
+		$(this).parent().addClass("active");
+	});
+
+	let total=$("nav > ul > li").length;
+	$("nav li li:last-child a").focusout(function(){
+		if($(this).parent().parent().parent().index() == total -1){
+			$("nav > ul").removeClass("active");
+		}
+		$(this).parent().parent().parent().removeClass("active");
+	});
 });
